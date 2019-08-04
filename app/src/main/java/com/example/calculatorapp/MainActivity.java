ad3d15e774ecc52ff8e31e03a7102a933a68c589
JAePortfolio <jarena000@citymail.cuny.edu>
2019-08-04 15:32:36 -0400

    Fixed bug where app would crash upon pressing an arithmetic button when no numbers were entered.

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -19,108 +20,128 @@ public class MainActivity extends AppCompatActivity {
     private int operandTwo = 0;
     private int operationID;
     private double result = 0;
+    private boolean operandsInputted = false;
     public String numDisplay = "";
 
     public void digitPress(View view) {
         switch (view.getId()) {
             case R.id.button_digitZero:
                 numDisplay = numDisplay + "0";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitOne:
                 numDisplay = numDisplay + "1";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitTwo:
                 numDisplay = numDisplay + "2";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitThree:
                 numDisplay = numDisplay + "3";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitFour:
                 numDisplay = numDisplay + "4";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitFive:
                 numDisplay = numDisplay + "5";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitSix:
                 numDisplay = numDisplay + "6";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitSeven:
                 numDisplay = numDisplay + "7";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitEight:
                 numDisplay = numDisplay + "8";
+                operandsInputted = true;
                 updateTextView();
                 break;
             case R.id.button_digitNine:
                 numDisplay = numDisplay + "9";
+                operandsInputted = true;
                 updateTextView();
                 break;
         }
     }
 
     public void action_arithPress(View view) {
-        switch (view.getId()) {
-            case R.id.button_arithAddition:
-                operandOne = Integer.parseInt(numDisplay); // Convert String to Int
-                numDisplay = "";
-                operationID = 0;
-                //updateTextView();
-                break;
-            case R.id.button_arithSubtraction:
-                operandOne = Integer.parseInt(numDisplay); // Convert String to Int
-                numDisplay = "";
-                operationID = 1;
-                //updateTextView();
-                break;
-            case R.id.button_arithMultiplication:
-                operandOne = Integer.parseInt(numDisplay); // Convert String to Int
-                numDisplay = "";
-                operationID = 2;
-                //updateTextView();
-                break;
-            case R.id.button_arithDivision:
-                operandOne = Integer.parseInt(numDisplay); // Convert String to Int
-                numDisplay = "";
-                operationID = 3;
-                //updateTextView();
-                break;
+        if(operandsInputted == true) {
+            switch (view.getId()) {
+                case R.id.button_arithAddition:
+                    operandOne = Integer.parseInt(numDisplay); // Convert String to Int
+                    numDisplay = "";
+                    operationID = 0;
+                    //updateTextView();
+                    break;
+                case R.id.button_arithSubtraction:
+                    operandOne = Integer.parseInt(numDisplay); // Convert String to Int
+                    numDisplay = "";
+                    operationID = 1;
+                    //updateTextView();
+                    break;
+                case R.id.button_arithMultiplication:
+                    operandOne = Integer.parseInt(numDisplay); // Convert String to Int
+                    numDisplay = "";
+                    operationID = 2;
+                    //updateTextView();
+                    break;
+                case R.id.button_arithDivision:
+                    operandOne = Integer.parseInt(numDisplay); // Convert String to Int
+                    numDisplay = "";
+                    operationID = 3;
+                    //updateTextView();
+                    break;
+            }
         }
+        else{}
     }
 
     public void equalsButton(View view){
-        switch (operationID){
-            case 0: // Addition
-                operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
-                result = operandOne + operandTwo;
-                numDisplay = Double.toString(result);
-                updateTextView();
-                break;
-            case 1: // Subtraction
-                operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
-                result = operandOne - operandTwo;
-                numDisplay = Double.toString(result);
-                updateTextView();
-                break;
-            case 2: // Multiplication
-                operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
-                result = operandOne * operandTwo;
-                numDisplay = Double.toString(result);
-                updateTextView();
-                break;
-            case 3: // Division
-                operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
-                result = operandOne / operandTwo;
-                numDisplay = Double.toString(result);
-                updateTextView();
-                break;
+        if(operandsInputted == true)
+            switch (operationID){
+                case 0: // Addition
+                    operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
+                    result = operandOne + operandTwo;
+                    numDisplay = Double.toString(result);
+                    updateTextView();
+                    break;
+                case 1: // Subtraction
+                    operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
+                    result = operandOne - operandTwo;
+                    numDisplay = Double.toString(result);
+                    updateTextView();
+                    break;
+                case 2: // Multiplication
+                    operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
+                    result = operandOne * operandTwo;
+                    numDisplay = Double.toString(result);
+                    updateTextView();
+                    break;
+                case 3: // Division
+                    operandTwo = Integer.parseInt(numDisplay); // Convert String to Int
+                    result = operandOne / operandTwo;
+                    numDisplay = Double.toString(result);
+                    updateTextView();
+                    break;
+            }
+        else{
+            Context context = getApplicationContext();
+            Toast errorMessage = Toast.makeText(context, "You didn't enter any numbers", Toast.LENGTH_SHORT);
+            errorMessage.show();
         }
     }
 
@@ -129,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
         operandTwo = 0;
         result = 0;
         numDisplay = "";
+        operandsInputted = false;
         updateTextView();
     }
 
